e836de5c96347a236bac01a61454da3d5e8b1637
Sarver, Edwin <edwin.sarver@keithley.com>
2024-04-04 11:52:18 -0400

    Add more keymaps, fix treesitter on windows.

--- lua/esarver/plugins/fugitive.lua
+++ lua/esarver/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    config = function() 
+    config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
         local Esarver_Fugitive = vim.api.nvim_create_augroup("Esarver_Fugitive", {})
@@ -15,24 +15,22 @@ return {
                 end
 
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
-                end, opts)
+                end, {buffer = bufnr, remap = false, desc="Fugitive: Push"})
 
-                -- rebase always
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
-                end, opts)
+                    vim.cmd.Git({'pull' })
+                end, {buffer = bufnr, remap = false, desc="Fugitive: Pull"})
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", {buffer = bufnr, remap = false, desc="Fugitive: Publish Branch"});
             end,
         })
 
 
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>", {desc = "Fugitive: Keep Upstream"})
+        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", {desc = "Fugitive: Keep HEAD"})
     end
 }
--- lua/esarver/plugins/harpoon.lua
+++ lua/esarver/plugins/harpoon.lua
@@ -2,13 +2,12 @@
 return {
     {
         'theprimeagen/harpoon',
-        config = function ()
-            vim.keymap.set("n", "<leader>a", function() require("harpoon.mark").add_file() end)
-            vim.keymap.set("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end)
-            vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end)
-            vim.keymap.set("n", "<C-t>", function() require("harpoon.ui").nav_file(2) end)
-            vim.keymap.set("n", "<C-n>", function() require("harpoon.ui").nav_file(3) end)
-            vim.keymap.set("n", "<C-s>", function() require("harpoon.ui").nav_file(4) end)
+        config = function () vim.keymap.set("n", "<leader>a", function() require("harpoon.mark").add_file() end, {desc="Harpoon: Set Mark"})
+            vim.keymap.set("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, {desc="Harpoon: Toggle Quick Menu"})
+            vim.keymap.set("n", "<leader><C-q>", function() require("harpoon.ui").nav_file(1) end)
+            vim.keymap.set("n", "<leader><C-w>", function() require("harpoon.ui").nav_file(2) end)
+            vim.keymap.set("n", "<leader><C-e>", function() require("harpoon.ui").nav_file(3) end)
+            vim.keymap.set("n", "<leader><C-r>", function() require("harpoon.ui").nav_file(4) end)
 
         end
     }
