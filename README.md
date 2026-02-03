d02a04990ee4a1a655e31001638392732766e6b3
Sarver, Edwin <edwin.sarver@keithley.com>
2026-02-03 14:44:17 -0500

    Added more common-for-me LSPs
    - Update README to include correct information about dependencies.

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # init.lua
 
-## Dependencies
+## Required Dependencies
 
-### LSP
-Any language server that you want will need to be installed and added to your
-`PATH`. This is to better support NixOS on my personal machine.
+- `fzf` (`pkgs.fzf`) Used for `telescope.nvim`
+- `ripgrep` (`pkgs.ripgrep`) Used for `telescope.nvim`
+- `gcc` or `clang` (`pkgs.gcc` or `pkgs.clang`) Used for `nvim-treesitter`
+- Neovim 0.12.0 or greater (use flake, TBD)
 
-#### LuaLS
+## Optional Dependencies
 
-Download the latest LuaLS from their [GitHub Releases](https://github.com/LuaLS/lua-language-server/releases/latest)
-
+### LSPs
+The following LSPs are fully configured and enabled, but are not required. It is
+recommended that you include them in your nix flake development tools for each project.
+- `clangd` C/C++
+- `cmake` CMake
+- `denols` Server-Side TS running on Deno
+- `jsonls` JSON
+- `just` Justfile
+- `lua_ls` Lua
+- `markdown_oxide` Markdown and PKMS
+- `nil_ls` Nix
+- `nixd` Nix
+- `pyright` --Python
+- `rust_analyzer` Rust
+- `taplo` TOML (rust)
+- `tinymist` Typst
+- `ts_ls` Typescript (node or browser)
+- `typos_lsp` Spell checker
+- `wgsl_analyzer` WGSL
+- `yamlls` YAML
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-            vim.opt.completeopt = {'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { desc = "LSP: Goto Definition" })
         end
@@ -11,13 +11,24 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- Show diagnostics on virtual lines when the current line has diagnostics to show.
-vim.diagnostic.config({virtual_lines = { current_line = true }})
+vim.diagnostic.config({ virtual_lines = { current_line = true } })
 
 -- Enable LSPs
 vim.lsp.enable({
-    "lua_ls",
-    "rust_analyzer",
-    "denols",
-    "markdown_oxide",
-    "tinymist",
+    "clangd",         -- C/C++
+    "cmake",          -- CMake
+    "denols",         -- Server-Side TS running on Deno
+    "jsonls",         -- JSON
+    "just",           -- Justfile
+    "lua_ls",         -- Lua
+    "markdown_oxide", -- Markdown and PKMS
+    "nixd",           -- Nix
+    "pyright",        --Python
+    "rust_analyzer",  -- Rust
+    "taplo",          -- TOML (rust)
+    "tinymist",       -- Typst
+    "ts_ls",          -- Typescript (node or browser)
+    "typos_lsp",      -- Spell checker
+    "wgsl_analyzer",  -- WGSL
+    "yamlls",         -- YAML
 })
